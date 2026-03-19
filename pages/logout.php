0125356bc2896d529e0c7098773c7663b15b927d
y4tin <yatoluan2@gmail.com>
2026-03-18 23:08:46 -0300

    Delete pages directory

--- pages/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Localiza a sessão atual
-session_unset(); // Limpa todas as variáveis da sessão
-session_destroy(); // Destrói a sessão completamente
-
-// Redireciona de volta para a página de login
-header("Location: login.html"); 
-exit();
-?>
